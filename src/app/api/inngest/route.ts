840abdf36df3edb6a84a6a2db61a9d25dc5e366d
Mohamed Reda Ali <m07hamedreda25@gmail.com>
2026-04-26 12:12:27 +0300

    Admin

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -3,7 +3,7 @@ import { inngest, functions } from "@/server/inngest";
 
 export const dynamic = "force-dynamic";
 
-export const maxDuration = 15;
+export const maxDuration = 30;
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
